316638e295824ffbaa9755b12e52539614a26176
Etheiry <144289258+Etheiry@users.noreply.github.com>
2025-03-10 12:28:33 -0400

    Finished

--- src/main/java/SkillBuilder4.java
+++ src/main/java/SkillBuilder4.java
@@ -10,9 +10,39 @@
 public class SkillBuilder4 {
 
     // replace this line with your constants
+    static int t_index;
+    static int y_index;
+    static boolean LOOKFORT;
+    static boolean  LOOKFORY;
+
+
 
     public static String findTYPattern(String s)
     {
-        // replace this line with your code
+        int index = 0;
+
+        LOOKFORT = true;
+
+        while (index < s.length()) {
+            if (s.toLowerCase().charAt(index) == 't' && LOOKFORT) {
+                t_index = index;
+                LOOKFORT = false;
+                LOOKFORY = true;
+            }
+
+            if (s.toLowerCase().charAt(index) == 'y' && LOOKFORY) {
+                y_index = index;
+                LOOKFORY = false;
+            }
+
+            if (!LOOKFORT && !LOOKFORY) {
+                return s.substring(t_index, y_index+1);
+            }
+
+            index++;
+        }
+
+
+        return "";
     }
 }
